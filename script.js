71fbfb0c701ec66a2609ff8b1514dd7265e38747
alpmumcuu <alp.mumcu@stu.ocsb.ca>
2024-06-13 14:23:57 -0400

    ss

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 /**
  * This file will get loaded by index.html
  */
+
+
+
+let clicks = 0;
+
+document.getElementById("btn_click").addEventListener("click", buttonClick);
+
+function buttonClick() {
+
+  clicks++;
+  document.getElementById("click_counter").textContent = clicks;
+}
+document.getElementById("btn_decrement").addEventListener("click", decrementClicks);
+document.getElementById("btn_reset").addEventListener("click", resetClicks);
+
+function decrementClicks() {
+  console.log("Decrement button clicked");
+  clicks--;
+  document.getElementById("click_counter").textContent = clicks;
+}
+
+function resetClicks() {
+  console.log("Reset button clicked");
+  clicks = 0;
+  document.getElementById("click_counter").textContent = clicks;
+}
